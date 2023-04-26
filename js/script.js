b8ee6ac82eae0798c922412167af70f6b8acb537
Anna <annarosewain9@gmail.com>
2023-04-26 14:31:34 +0100

    Linked city select to display details of selected city

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
-function updateTime() {
-  //new york
-  let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElement = document.querySelector("#ny-date");
-  let newYorkTimeElement = document.querySelector("#ny-time");
-  let newYorkTime = moment()
-    .tz("America/New_York")
-    .format("h:mm:ss:SS [<small>]A[</small>]");
-  let newYorkDate = moment().tz("America/New_York").format("MMMM Do YYYY");
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace(`_`, `/`).split(`/`)[1];
+  let cityTime = moment().tz(`${cityTimeZone}`);
 
-  newYorkDateElement.innerHTML = newYorkDate;
-  newYorkTimeElement.innerHTML = newYorkTime;
-  //Amsterdam
-  let amsterdamElement = document.querySelector("#amsterdam");
-  let amsterdamDateElement = document.querySelector("#amsterdam-date");
-  let amsterdamTimeElement = document.querySelector("#amsterdam-time");
-  let amsterdamTime = moment()
-    .tz("Europe/Amsterdam")
-    .format("h:mm:ss:SS [<small>]A[</small>]");
-  let amsterdamDate = moment().tz("Europe/Amsterdam").format("MMMM Do YYYY");
-
-  amsterdamDateElement.innerHTML = amsterdamDate;
-  amsterdamTimeElement.innerHTML = amsterdamTime;
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city" id="new-york">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div>
+            <div class="time">${cityTime.format(
+              "h:mm:ss:SS [<small>]A[</small>]"
+            )}</div>
+          </div>
+        </div>`;
 }
-setInterval(updateTime, 1);
+
+let citySelectElement = document.querySelector("#city-select");
+citySelectElement.addEventListener("change", updateCity);
