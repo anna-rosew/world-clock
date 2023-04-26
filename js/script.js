c3f03b83fc0710f2d83b70b3152127dcd92ce97e
Anna <annarosewain9@gmail.com>
2023-04-26 13:40:48 +0100

    Connected JS to moment.js to show real time in locations

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+function updateTime() {
+  //new york
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = document.querySelector("#ny-date");
+  let newYorkTimeElement = document.querySelector("#ny-time");
+  let newYorkTime = moment()
+    .tz("America/New_York")
+    .format("h:mm:ss:SS [<small>]A[</small>]");
+  let newYorkDate = moment().tz("America/New_York").format("MMMM Do YYYY");
 
+  newYorkDateElement.innerHTML = newYorkDate;
+  newYorkTimeElement.innerHTML = newYorkTime;
+  //Amsterdam
+  let amsterdamElement = document.querySelector("#amsterdam");
+  let amsterdamDateElement = document.querySelector("#amsterdam-date");
+  let amsterdamTimeElement = document.querySelector("#amsterdam-time");
+  let amsterdamTime = moment()
+    .tz("Europe/Amsterdam")
+    .format("h:mm:ss:SS [<small>]A[</small>]");
+  let amsterdamDate = moment().tz("Europe/Amsterdam").format("MMMM Do YYYY");
+
+  amsterdamDateElement.innerHTML = amsterdamDate;
+  amsterdamTimeElement.innerHTML = amsterdamTime;
+}
+setInterval(updateTime, 1);
